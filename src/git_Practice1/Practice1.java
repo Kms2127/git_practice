bf6514586c70da8828c063f14d6c30b9c7632142
kms <godloveskidist@gmail.com>
2022-01-10 15:54:34 -0500

    work 4 is done

--- src/git_Practice1/Practice1.java
+++ src/git_Practice1/Practice1.java
@@ -4,6 +4,8 @@ public class Practice1 {
     public static void main(String[] args) {
         System.out.println("I am eating a lot of chocolate");
     }
+    //master : local
+    //origin & master : remote
     //commit practice
     //commit practice 2
     //commit practice 3
